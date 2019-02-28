ff867aa3efd4047dfbb361b50534f48e9e54254c
Jérémy Barbet <jeremgraph@gmail.com>
2019-02-28 16:02:09 +0000

    Clean plugins

--- next.config.js
+++ next.config.js
@@ -1,38 +1,43 @@
 const path = require('path');
-const withSass = require('@zeit/next-sass');
-const withTypescript = require('@zeit/next-typescript');
+const sass = require('@zeit/next-sass');
+const typescript = require('@zeit/next-typescript');
+const plugins = require('next-compose-plugins');
+const images = require('next-images');
+const reactSvg = require('next-react-svg');
 
-module.exports = withTypescript(
-  withSass({
+const nextConfig = {
+  webpack(config) {
+    const classNamesLoader = require.resolve('next-classnames-loader');
+    const styleRules = config.module.rules.filter(rule => rule.test.test('file.scss') || rule.test.test('file.sass'));
+
+    styleRules.forEach(styleRule => {
+      if (styleRule.use && styleRule.use.indexOf(classNamesLoader) === -1) {
+        styleRule.use.splice(0, 0, classNamesLoader);
+      }
+    });
+
+    config.resolve = config.resolve || {};
+
+    config.resolve.modules = [
+      path.join(__dirname, 'src'),
+      path.join(__dirname, 'node_modules'),
+    ];
+
+    return config;
+  },
+};
+
+module.exports = plugins([
+  [sass, {
     cssModules: true,
     cssLoaderOptions: {
       importLoaders: 1,
       localIdentName: "[local]___[hash:base64:5]",
     },
-    webpack(config) {
-      const classNamesLoader = require.resolve('next-classnames-loader');
-      const styleRules = config.module.rules.filter(rule => rule.test.test('file.scss') || rule.test.test('file.sass'));
+  }],
 
-      styleRules.forEach(styleRule => {
-        if (styleRule.use && styleRule.use.indexOf(classNamesLoader) === -1) {
-          styleRule.use.splice(0, 0, classNamesLoader);
-        }
-      });
+  [images, { exclude: path.resolve(__dirname, 'src/assets/svg') }],
+  [reactSvg, { include: path.resolve(__dirname, 'src/assets/svg') }],
 
-      config.resolve = config.resolve || {};
-
-      config.resolve.modules = [
-        path.join(__dirname, 'src'),
-        path.join(__dirname, 'node_modules'),
-      ];
-
-      config.module.rules.push({
-        test: /\.(svg)$/,
-        include: /assets\/svg/,
-        loader: require.resolve('svg-react-loader'),
-      });
-
-      return config;
-    }
-  })
-);
+  typescript,
+], nextConfig);
